7fbf243047274c50670dbefb551d946ff2f0a16d
swathi-india <68546712+swathi-india@users.noreply.github.com>
2022-07-23 15:50:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,90 @@
+//require the installed node modules
+
+const bodyParser = require("body-parser");
+
+const express = require("express");
+
+const request = require("request");
+
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public")); //to make our page dynamic
+app.use(bodyParser.urlencoded({extended:true}));
+
+//to get request from webbrowser
+app.get("/",function(req,res) {
+
+res.sendFile(__dirname + "/signup.html");
+
+});
+
+
+app.post("/",function(req,res) {
+const firstName = req.body.fname;
+const lastName = req.body.lname;
+const email = req.body.email;
+const data = {
+  members:[
+    {
+      email_address:email,
+      status:"subscribed",
+      merge_fields:{
+        FNAME:firstName,
+        LNAME:lastName
+      }
+    }
+  ]
+};
+
+const jsonData = JSON.stringify(data);
+
+const url =  "https://us11.api.mailchimp.com/3.0/lists/5007ac4f47";
+
+const options = {
+
+method:"POST",
+auth:"swathi2:43849ffb925ed490456ef5ddb82c7149-us11"
+
+}
+
+
+const request = https.request(url,options,function(response) {
+
+  if(response.statusCode === 200){
+
+      res.sendFile( __dirname + "/success.html");
+
+  }
+
+  else{
+
+    res.sendFile(__dirname + "/failure.html");
+  }
+  response.on("data",function(data) {
+    console.log(JSON.parse(data));
+  });
+
+});
+
+request.write(jsonData);
+request.end();
+});
+
+app.post("/failure",function(req,res) {
+  res.redirect("/");
+});
+
+
+// this is used to listen to port
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running on port 3000");
+});
+
+
+// apikey
+// 43849ffb925ed490456ef5ddb82c7149-us11
+
+// listid
+//  5007ac4f47
